e24bf2ecf3031a77c380aa1db6a55ab59ab933ad
nesterenko-marina99 <miss.nesterenko.marina@gmail.com>
2021-07-01 12:32:58 +0400

    4_2_4 task done

---
+++ pages/base_page.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+
+class BasePage():
+	def __init__(self, browser, url):
+		self.browser = browser
+		self.url = url
+
+	def open(self):
+		self.browser.get(self.url)
+	
+
+
